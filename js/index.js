f5774fbde2da7902d1ee52ba68ccbaef6a77859a
kimmerrmyauw <kimmyauwcanva@gmail.com>
2026-08-28 14:52:05 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Special just for my boyfriend 🤍`.split('')
+const text = `Special just for my boyfriend`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
