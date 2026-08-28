0dc6d1b5218219ed435cc24e6e77b1ba52cb35c2
kimmerrmyauw <kimmyauwcanva@gmail.com>
2026-08-28 14:37:39 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('i know that people who come will go, but i hope we remain friends until we grow old').split('')
+    const titles = ('I’m so proud of how hard you work every day. Even when you’re busy, you still make time to treat me with love and care. Thank you for always making an effort, even in the little things. Please remember to take care of yourself and get enough rest. I love you so much, and I’ll always be here cheering you on. 🤍').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
